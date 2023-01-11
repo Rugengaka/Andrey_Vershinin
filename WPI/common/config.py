8f08c594cd9eda4846753564b79745922135f8cb
Rugengaka <120188046+Rugengaka@users.noreply.github.com>
2023-01-11 22:54:01 +0200

    Create config.py

---
+++ WPI/common/config.py
@@ -0,0 +1,3 @@
+ACCESS_TOKEN = 'sl.BWsR7uw_bg-2q8TIqVCHLfUTPVrd3kfpGdpKe4lB1rzf6XfNXZmKb42rE68XxoMzxHA_PPLEr65xk_tb5QYBlWdpP4BgCctd7cqE7fNoAKZLYm8HJTkkicjyy-fidshUFTxAKHE'
+FILE_TO_UPLOAD = 'photo.jpg'
+RELATIVE_PATH_TO_FILE = f'resources/{FILE_TO_UPLOAD}'
